1fe884813b70acc2a67eaf1a09bcbe143c9b748e
anvarovic576 <dalerjalolov668@gmail.com>
2026-08-05 20:52:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+function darkMode() {
+  document.body.classList.toggle("dark");
+}
+
+
+function xabar() {
+  alert("Yangilik to‘liq tez orada qo‘shiladi!");
+}
+
+
 function likeNews() {
-    alert("Yangilikni o‘qiganingiz uchun rahmat!");
+  alert("👍 Rahmat! Sizga yoqdi.");
 }
 
-function searchNews() {
-    let input = document.getElementById("search").value.toLowerCase();
-    let news = document.querySelectorAll(".news");
 
-    news.forEach(function(item) {
-        if (item.innerText.toLowerCase().includes(input)) {
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
-    });
-}
+// Qidiruv
+
+let search = document.getElementById("search");
+
+search.addEventListener("keyup", function() {
+
+  let text = search.value.toLowerCase();
+
+  let news = document.querySelectorAll(".news");
+
+  news.forEach(function(item) {
+
+    let content = item.innerText.toLowerCase();
+
+    if(content.includes(text)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+
+  });
+
+});
